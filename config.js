e6a3a2236cc079c7395eaf7f76dbe07724f91b58
Roderick Gadellaa <roderick.gadellaa@brightside.nl>
2019-03-15 14:58:46 +0100

    Fix: load config.json, not config-example.json

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ var conf = convict({
 // Ugly part about loading config.
 // Check if there is a local config
 try {
-  conf.loadFile('./config-example.json');
+  conf.loadFile('./config.json');
 } catch(err) {}
 
 // If we run with docker we want to link a config folder.
